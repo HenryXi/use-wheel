86041e3c5b069989ec4a9790ecd0d2aa6b18245d
yong <xxy668@foxmail.com>
2016-06-27 21:55:35 +0800

    do not go for perfect

---
+++ apache-common-lang3/src/main/java/com/henryxi/apache/common/lang3/ProxyStringUtils.java
@@ -0,0 +1,1008 @@
+package com.henryxi.apache.common.lang3;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.nio.charset.Charset;
+import java.util.Iterator;
+import java.util.Locale;
+
+public class ProxyStringUtils {
+    public String remove(String val0, String val1) {
+
+        return null;
+    }
+
+    public String remove(String val0, char val1) {
+
+        return null;
+    }
+
+    public boolean equals(CharSequence val0, CharSequence val1) {
+
+        return false;
+    }
+
+    public String toString(byte[] val0, String val1) {
+
+        return null;
+    }
+
+    public int indexOf(CharSequence val0, int val1) {
+
+        return 0;
+    }
+
+    public int indexOf(CharSequence val0, int val1, int val2) {
+
+        return 0;
+    }
+
+    public int indexOf(CharSequence val0, CharSequence val1, int val2) {
+
+        return 0;
+    }
+
+    public int indexOf(CharSequence val0, CharSequence val1) {
+
+        return 0;
+    }
+
+    public boolean contains(CharSequence val0, int val1) {
+
+        return false;
+    }
+
+    public boolean contains(CharSequence val0, CharSequence val1) {
+
+        return false;
+    }
+
+    public boolean endsWith(CharSequence val0, CharSequence val1) {
+
+        return false;
+    }
+
+    public boolean endsWith(CharSequence val0, CharSequence val1, boolean val2) {
+
+        return false;
+    }
+
+    public boolean equalsIgnoreCase(CharSequence val0, CharSequence val1) {
+
+        return false;
+    }
+
+    public boolean isEmpty(CharSequence val0) {
+
+        return false;
+    }
+
+    public String join(char[] val0, char val1) {
+
+        return null;
+    }
+
+    public String join(float[] val0, char val1) {
+
+        return null;
+    }
+
+    public String join(double[] val0, char val1) {
+
+        return null;
+    }
+
+    public String join(long[] val0, char val1, int val2, int val3) {
+
+        return null;
+    }
+
+    public String join(Object[] val0) {
+
+        return null;
+    }
+
+    public String join(Object[] val0, char val1) {
+
+        return null;
+    }
+
+    public String join(long[] val0, char val1) {
+
+        return null;
+    }
+
+    public String join(int[] val0, char val1) {
+
+        return null;
+    }
+
+    public String join(short[] val0, char val1) {
+
+        return null;
+    }
+
+    public String join(byte[] val0, char val1) {
+
+        return null;
+    }
+
+    public String join(double[] val0, char val1, int val2, int val3) {
+
+        return null;
+    }
+
+    public String join(char[] val0, char val1, int val2, int val3) {
+
+        return null;
+    }
+
+    public String join(short[] val0, char val1, int val2, int val3) {
+
+        return null;
+    }
+
+    public String join(byte[] val0, char val1, int val2, int val3) {
+
+        return null;
+    }
+
+    public String join(float[] val0, char val1, int val2, int val3) {
+
+        return null;
+    }
+
+    public String join(Object[] val0, String val1) {
+
+        return null;
+    }
+
+    public String join(Object[] val0, String val1, int val2, int val3) {
+
+        return null;
+    }
+
+    public String join(Iterable val0, String val1) {
+
+        return null;
+    }
+
+    public String join(Iterable val0, char val1) {
+
+        return null;
+    }
+
+    public String join(Iterator val0, String val1) {
+
+        return null;
+    }
+
+    public String join(Object[] val0, char val1, int val2, int val3) {
+
+        return null;
+    }
+
+    public String join(Iterator val0, char val1) {
+
+        return null;
+    }
+
+    public String join(int[] val0, char val1, int val2, int val3) {
+
+        return null;
+    }
+
+    public int lastIndexOf(CharSequence val0, int val1) {
+
+        return 0;
+    }
+
+    public int lastIndexOf(CharSequence val0, CharSequence val1) {
+
+        return 0;
+    }
+
+    public int lastIndexOf(CharSequence val0, CharSequence val1, int val2) {
+
+        return 0;
+    }
+
+    public int lastIndexOf(CharSequence val0, int val1, int val2) {
+
+        return 0;
+    }
+
+    public int length(CharSequence val0) {
+
+        return 0;
+    }
+
+    public String replace(String val0, String val1, String val2) {
+
+        return null;
+    }
+
+    public String replace(String val0, String val1, String val2, int val3) {
+
+        return null;
+    }
+
+    public String[] split(String val0, char val1) {
+
+        return null;
+    }
+
+    public String[] split(String val0) {
+
+        return null;
+    }
+
+    public String[] split(String val0, String val1) {
+
+        return null;
+    }
+
+    public String[] split(String val0, String val1, int val2) {
+
+        return null;
+    }
+
+    public boolean startsWith(CharSequence val0, CharSequence val1, boolean val2) {
+
+        return false;
+    }
+
+    public boolean startsWith(CharSequence val0, CharSequence val1) {
+
+        return false;
+    }
+
+    public String substring(String val0, int val1, int val2) {
+
+        return null;
+    }
+
+    public String substring(String val0, int val1) {
+
+        return null;
+    }
+
+    public String trim(String val0) {
+
+        return null;
+    }
+
+    public String wrap(String val0, char val1) {
+
+        return null;
+    }
+
+    public String wrap(String val0, String val1) {
+
+        return null;
+    }
+
+    public String reverse(String val0) {
+
+        return null;
+    }
+
+    public boolean isWhitespace(CharSequence val0) {
+
+        return false;
+    }
+
+    public String left(String val0, int val1) {
+
+        return null;
+    }
+
+    public String right(String val0, int val1) {
+
+        return null;
+    }
+
+    public boolean isAlpha(CharSequence val0) {
+
+        return false;
+    }
+
+    public boolean isNumeric(CharSequence val0) {
+
+        return false;
+    }
+
+    public String[] splitByWholeSeparatorPreserveAllTokens(String val0, String val1) {
+
+        return null;
+    }
+
+    public String[] splitByWholeSeparatorPreserveAllTokens(String val0, String val1, int val2) {
+
+        return null;
+    }
+
+    public int indexOfIgnoreCase(CharSequence val0, CharSequence val1, int val2) {
+
+        return 0;
+    }
+
+    public int indexOfIgnoreCase(CharSequence val0, CharSequence val1) {
+
+        return 0;
+    }
+
+    public int lastOrdinalIndexOf(CharSequence val0, CharSequence val1, int val2) {
+
+        return 0;
+    }
+
+    public int lastIndexOfIgnoreCase(CharSequence val0, CharSequence val1, int val2) {
+
+        return 0;
+    }
+
+    public int lastIndexOfIgnoreCase(CharSequence val0, CharSequence val1) {
+
+        return 0;
+    }
+
+    public boolean containsIgnoreCase(CharSequence val0, CharSequence val1) {
+
+        return false;
+    }
+
+    public boolean containsWhitespace(CharSequence val0) {
+
+        return false;
+    }
+
+    public String substringBeforeLast(String val0, String val1) {
+
+        return null;
+    }
+
+    public String substringAfterLast(String val0, String val1) {
+
+        return null;
+    }
+
+    public String[] substringsBetween(String val0, String val1, String val2) {
+
+        return null;
+    }
+
+    public String[] splitByWholeSeparator(String val0, String val1) {
+
+        return null;
+    }
+
+    public String[] splitByWholeSeparator(String val0, String val1, int val2) {
+
+        return null;
+    }
+
+    public String[] splitByWholeSeparatorWorker(String val0, String val1, int val2, boolean val3) {
+
+        return null;
+    }
+
+    public String[] splitPreserveAllTokens(String val0, String val1, int val2) {
+
+        return null;
+    }
+
+    public String[] splitPreserveAllTokens(String val0, String val1) {
+
+        return null;
+    }
+
+    public String[] splitPreserveAllTokens(String val0, char val1) {
+
+        return null;
+    }
+
+    public String[] splitPreserveAllTokens(String val0) {
+
+        return null;
+    }
+
+    public String[] splitByCharacterType(String val0) {
+
+        return null;
+    }
+
+    public String[] splitByCharacterType(String val0, boolean val1) {
+
+        return null;
+    }
+
+    public String[] splitByCharacterTypeCamelCase(String val0) {
+
+        return null;
+    }
+
+    public String removeStartIgnoreCase(String val0, String val1) {
+
+        return null;
+    }
+
+    public String removeEndIgnoreCase(String val0, String val1) {
+
+        return null;
+    }
+
+    public String replaceEachRepeatedly(String val0, String[] val1, String[] val2) {
+
+        return null;
+    }
+
+    public boolean isAlphanumericSpace(CharSequence val0) {
+
+        return false;
+    }
+
+    public int indexOfDifference(CharSequence[] val0) {
+
+        return 0;
+    }
+
+    public int indexOfDifference(CharSequence val0, CharSequence val1) {
+
+        return 0;
+    }
+
+    public int getLevenshteinDistance(CharSequence val0, CharSequence val1, int val2) {
+
+        return 0;
+    }
+
+    public int getLevenshteinDistance(CharSequence val0, CharSequence val1) {
+
+        return 0;
+    }
+
+    public double getJaroWinklerDistance(CharSequence val0, CharSequence val1) {
+
+        return 0;
+    }
+
+    public String[] splitWorker(String val0, String val1, int val2, boolean val3) {
+
+        return null;
+    }
+
+    public String[] splitWorker(String val0, char val1, boolean val2) {
+
+        return null;
+    }
+
+    public String deleteWhitespace(String val0) {
+
+        return null;
+    }
+
+    public String removeStart(String val0, String val1) {
+
+        return null;
+    }
+
+    public String replaceOnce(String val0, String val1, String val2) {
+
+        return null;
+    }
+
+    public String replacePattern(String val0, String val1, String val2) {
+
+        return null;
+    }
+
+    public String removePattern(String val0, String val1) {
+
+        return null;
+    }
+
+    public String replaceEach(String val0, String[] val1, String[] val2, boolean val3, int val4) {
+
+        return null;
+    }
+
+    public String replaceEach(String val0, String[] val1, String[] val2) {
+
+        return null;
+    }
+
+    public String repeat(char val0, int val1) {
+
+        return null;
+    }
+
+    public String repeat(String val0, int val1) {
+
+        return null;
+    }
+
+    public String repeat(String val0, String val1, int val2) {
+
+        return null;
+    }
+
+    public String replaceChars(String val0, char val1, char val2) {
+
+        return null;
+    }
+
+    public String replaceChars(String val0, String val1, String val2) {
+
+        return null;
+    }
+
+    public String overlay(String val0, String val1, int val2, int val3) {
+
+        return null;
+    }
+
+    public String chomp(String val0) {
+
+        return null;
+    }
+
+    public String chomp(String val0, String val1) {
+
+        return null;
+    }
+
+    public String chop(String val0) {
+
+        return null;
+    }
+
+    public String rightPad(String val0, int val1, String val2) {
+
+        return null;
+    }
+
+    public String rightPad(String val0, int val1, char val2) {
+
+        return null;
+    }
+
+    public String rightPad(String val0, int val1) {
+
+        return null;
+    }
+
+    public String leftPad(String val0, int val1, char val2) {
+
+        return null;
+    }
+
+    public String leftPad(String val0, int val1, String val2) {
+
+        return null;
+    }
+
+    public String leftPad(String val0, int val1) {
+
+        return null;
+    }
+
+    public String center(String val0, int val1, char val2) {
+
+        return null;
+    }
+
+    public String center(String val0, int val1) {
+
+        return null;
+    }
+
+    public String center(String val0, int val1, String val2) {
+
+        return null;
+    }
+
+    public String upperCase(String val0) {
+
+        return null;
+    }
+
+    public String upperCase(String val0, Locale val1) {
+
+        return null;
+    }
+
+    public String lowerCase(String val0) {
+
+        return null;
+    }
+
+    public String lowerCase(String val0, Locale val1) {
+
+        return null;
+    }
+
+    public String capitalize(String val0) {
+
+        return null;
+    }
+
+    public String uncapitalize(String val0) {
+
+        return null;
+    }
+
+    public String swapCase(String val0) {
+
+        return null;
+    }
+
+    public int countMatches(CharSequence val0, char val1) {
+
+        return 0;
+    }
+
+    public int countMatches(CharSequence val0, CharSequence val1) {
+
+        return 0;
+    }
+
+    public boolean isAlphaSpace(CharSequence val0) {
+
+        return false;
+    }
+
+    public boolean isAlphanumeric(CharSequence val0) {
+
+        return false;
+    }
+
+    public boolean isAsciiPrintable(CharSequence val0) {
+
+        return false;
+    }
+
+    public boolean isNumericSpace(CharSequence val0) {
+
+        return false;
+    }
+
+    public boolean isAllLowerCase(CharSequence val0) {
+
+        return false;
+    }
+
+    public boolean isAllUpperCase(CharSequence val0) {
+
+        return false;
+    }
+
+    public String defaultString(String val0) {
+
+        return null;
+    }
+
+    public String defaultString(String val0, String val1) {
+
+        return null;
+    }
+
+    public CharSequence defaultIfBlank(CharSequence val0, CharSequence val1) {
+
+        return null;
+    }
+
+    public CharSequence defaultIfEmpty(CharSequence val0, CharSequence val1) {
+
+        return null;
+    }
+
+    public String reverseDelimited(String val0, char val1) {
+
+        return null;
+    }
+
+    public String abbreviate(String val0, int val1, int val2) {
+
+        return null;
+    }
+
+    public String abbreviate(String val0, int val1) {
+
+        return null;
+    }
+
+    public String abbreviateMiddle(String val0, String val1, int val2) {
+
+        return null;
+    }
+
+    public String difference(String val0, String val1) {
+
+        return null;
+    }
+
+    public String getCommonPrefix(String[] val0) {
+
+        return null;
+    }
+
+    public double score(CharSequence val0, CharSequence val1) {
+
+        return 0;
+    }
+
+    public int transpositions(CharSequence val0, CharSequence val1) {
+
+        return 0;
+    }
+
+    public int getFuzzyDistance(CharSequence val0, CharSequence val1, Locale val2) {
+
+        return 0;
+    }
+
+    public String getSetOfMatchingCharacterWithin(CharSequence val0, CharSequence val1, int val2) {
+
+        return null;
+    }
+
+    public int commonPrefixLength(CharSequence val0, CharSequence val1) {
+
+        return 0;
+    }
+
+    public boolean startsWithIgnoreCase(CharSequence val0, CharSequence val1) {
+
+        return false;
+    }
+
+    public boolean endsWithIgnoreCase(CharSequence val0, CharSequence val1) {
+
+        return false;
+    }
+
+    public String appendIfMissingIgnoreCase(String val0, CharSequence val1, CharSequence[] val2) {
+
+        return null;
+    }
+
+    public String prependIfMissingIgnoreCase(String val0, CharSequence val1, CharSequence[] val2) {
+
+        return null;
+    }
+
+    public boolean startsWithAny(CharSequence val0, CharSequence[] val1) {
+
+        return false;
+    }
+
+    public String normalizeSpace(String val0) {
+
+        return null;
+    }
+
+    public boolean endsWithAny(CharSequence val0, CharSequence[] val1) {
+
+        return false;
+    }
+
+    public String appendIfMissing(String val0, CharSequence val1, boolean val2, CharSequence[] val3) {
+
+        return null;
+    }
+
+    public String appendIfMissing(String val0, CharSequence val1, CharSequence[] val2) {
+
+        return null;
+    }
+
+    public String prependIfMissing(String val0, CharSequence val1, CharSequence[] val2) {
+
+        return null;
+    }
+
+    public String prependIfMissing(String val0, CharSequence val1, boolean val2, CharSequence[] val3) {
+
+        return null;
+    }
+
+    public String toEncodedString(byte[] val0, Charset val1) {
+
+        return null;
+    }
+
+    public String removeEnd(String val0, String val1) {
+        return StringUtils.removeEnd("asdfasdfasdf99", "99");
+    }
+
+    public int indexOfAny(CharSequence val0, CharSequence[] val1) {
+
+        return 0;
+    }
+
+    public int indexOfAny(CharSequence val0, String val1) {
+
+        return 0;
+    }
+
+    public int indexOfAny(CharSequence val0, char[] val1) {
+
+        return 0;
+    }
+
+    public boolean isNotEmpty(CharSequence val0) {
+
+        return false;
+    }
+
+    public boolean isAnyEmpty(CharSequence[] val0) {
+
+        return false;
+    }
+
+    public boolean isNoneEmpty(CharSequence[] val0) {
+
+        return false;
+    }
+
+    public boolean isBlank(CharSequence val0) {
+
+        return false;
+    }
+
+    public boolean isNotBlank(CharSequence val0) {
+
+        return false;
+    }
+
+    public boolean isAnyBlank(CharSequence[] val0) {
+
+        return false;
+    }
+
+    public boolean isNoneBlank(CharSequence[] val0) {
+
+        return false;
+    }
+
+    public String trimToNull(String val0) {
+
+        return null;
+    }
+
+    public String trimToEmpty(String val0) {
+
+        return null;
+    }
+
+    public String stripAccents(String val0) {
+
+        return null;
+    }
+
+    public String strip(String val0, String val1) {
+
+        return null;
+    }
+
+    public String strip(String val0) {
+
+        return null;
+    }
+
+    public String stripToNull(String val0) {
+
+        return null;
+    }
+
+    public String stripToEmpty(String val0) {
+
+        return null;
+    }
+
+    public String stripStart(String val0, String val1) {
+
+        return null;
+    }
+
+    public String stripEnd(String val0, String val1) {
+
+        return null;
+    }
+
+    public String[] stripAll(String[] val0) {
+
+        return null;
+    }
+
+    public String[] stripAll(String[] val0, String val1) {
+
+        return null;
+    }
+
+    public int ordinalIndexOf(CharSequence val0, CharSequence val1, int val2) {
+
+        return 0;
+    }
+
+    public int ordinalIndexOf(CharSequence val0, CharSequence val1, int val2, boolean val3) {
+
+        return 0;
+    }
+
+    public boolean containsAny(CharSequence val0, CharSequence val1) {
+
+        return false;
+    }
+
+    public boolean containsAny(CharSequence val0, char[] val1) {
+
+        return false;
+    }
+
+    public boolean containsAny(CharSequence val0, CharSequence[] val1) {
+
+        return false;
+    }
+
+    public int indexOfAnyBut(CharSequence val0, char[] val1) {
+
+        return 0;
+    }
+
+    public int indexOfAnyBut(CharSequence val0, CharSequence val1) {
+
+        return 0;
+    }
+
+    public boolean containsOnly(CharSequence val0, char[] val1) {
+
+        return false;
+    }
+
+    public boolean containsOnly(CharSequence val0, String val1) {
+
+        return false;
+    }
+
+    public boolean containsNone(CharSequence val0, char[] val1) {
+
+        return false;
+    }
+
+    public boolean containsNone(CharSequence val0, String val1) {
+
+        return false;
+    }
+
+    public int lastIndexOfAny(CharSequence val0, CharSequence[] val1) {
+
+        return 0;
+    }
+
+    public String mid(String val0, int val1, int val2) {
+
+        return null;
+    }
+
+    public String substringBefore(String val0, String val1) {
+
+        return null;
+    }
+
+    public String substringAfter(String val0, String val1) {
+
+        return null;
+    }
+
+    public String substringBetween(String val0, String val1) {
+
+        return null;
+    }
+
+    public String substringBetween(String val0, String val1, String val2) {
+
+        return null;
+    }
+}
