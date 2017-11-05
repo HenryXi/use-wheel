c5f16c49ac787aa5c5f5e47f8ec8dd018c798f8b
yong <xixiaoyong@xiaomi.com>
2017-11-05 20:12:20 +0800

    apache common stringUtils subString test

---
+++ apache-common-lang3/src/main/java/com/henryxi/apache/common/lang3/StringUtilsSubStringClient.java
@@ -0,0 +1,11 @@
+package com.henryxi.apache.common.lang3;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class StringUtilsSubStringClient {
+    public static void main(String[] args) {
+        String str="https://img1.doubanio.com/mpic/s4237197.jpg";
+        String imageId = StringUtils.substringBetween(str, "/s", ".jpg");
+        System.out.println(imageId);
+    }
+}
