3ca448cc587de871d319fbeb534927dcb8128996
yong <xixiaoyong@xiaomi.com>
2020-04-26 10:14:55 +0800

    this is auto commit message

--- guava/src/main/java/com/henryxi/guava/cache/ObjectCacheClient.java
+++ guava/src/main/java/com/henryxi/guava/cache/ObjectCacheClient.java
@@ -1,4 +1,4 @@
-package com.henryxi.guava;
+package com.henryxi.guava.cache;
 
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
@@ -7,27 +7,31 @@ import org.apache.commons.lang3.StringUtils;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
-public class CacheClient {
+public class ObjectCacheClient {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         long timestamp = System.currentTimeMillis();
-        Cache<String, String> localCache = CacheBuilder.newBuilder().expireAfterWrite(1, TimeUnit.SECONDS).build();
-        String firstGet = localCache.get("key", () -> getDataFromDB("key"));
+        Cache<String, User> localCache = CacheBuilder.newBuilder().expireAfterWrite(60, TimeUnit.SECONDS).build();
+        User firstGet = localCache.get("key", () -> getDataFromDB("key"));
         System.out.println("firstGet:" + firstGet + ",time cost:" + (System.currentTimeMillis() - timestamp));
         timestamp = System.currentTimeMillis();
-        String secondGet = localCache.get("key", () -> getDataFromDB("key"));
+        firstGet.setName("change new name");
+        User secondGet = localCache.get("key", () -> getDataFromDB("key"));
         System.out.println("secondGet:" + secondGet + ",time cost:" + (System.currentTimeMillis() - timestamp));
         Thread.sleep(3000);
         timestamp = System.currentTimeMillis();
-        String thirdGet = localCache.get("key", () -> getDataFromDB("key"));
+        User thirdGet = localCache.get("key", () -> getDataFromDB("key"));
         System.out.println("thirdGet:" + thirdGet + ",time cost:" + (System.currentTimeMillis() - timestamp));
 
     }
 
-    private static String getDataFromDB(String key) throws InterruptedException {
+    private static User getDataFromDB(String key) throws InterruptedException {
         System.out.println("get data from database...");
         Thread.sleep(2000);
         if (StringUtils.equals(key, "key")) {
-            return "data";
+            User user = new User();
+            user.setName("henry");
+            user.setAge(32);
+            return user;
         }
         return null;
     }
